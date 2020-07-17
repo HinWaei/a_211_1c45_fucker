ad06d08a65a477900e2d3a5c3921a08027a68791
HinWaei <1624339284@qq.com>
2020-07-17 23:40:43 +0800

    master

--- bayes/dataset/data_set_generator.py
+++ bayes/dataset/data_set_generator.py
@@ -18,24 +18,30 @@ class data_set_generator:
 
         #try:
         with open(file_path,"r") as f:
-        #Here I don't wanna use f.read() directly because loading the whole passage into memory would take great size of space
-            line=f.readline() 
-            while line:
-                s=[]
-                sentences=[]                       #s:Each line with separated sentences.         sentences:Made up of elements except the one with fragment sentence of s
-                #"line" is reserved in order to get fragment sentence(i.e. a sentence that not ended by period symbol(.) )
-                s=line.split(".")                       #Split each line with '.' to get single sentences
-                s[0]+=frag
-                sentences+=s[:-1]
-                for i in range(len(sentences)):
-                    sentences[i]+='\n'               
-                frag=s[-1] if s[-1]!='.' else ""             #If the line is not ended by period(.) then the last element would be the fragment sentence
-                line=f.readline()
+            with open("./vector.txt","w+") as ds:
+            #Here I don't wanna use f.read() directly because loading the whole passage into memory would take great size of space
+                line=f.readline() 
+                while line:
+                    s=[]
+                    sentences=[]                       #s:Each line with separated sentences.         sentences:Made up of elements except the one with fragment sentence of s
+                    #"line" is reserved in order to get fragment sentence(i.e. a sentence that not ended by period symbol(.) )
+                    s=line.split(".")                       #Split each line with '.' to get single sentences
+                    s[0]+=frag
+                    sentences+=s[:-1]               
+                    frag=s[-1] if s[-1]!='.' else ""             #If the line is not ended by period(.) then the last element would be the fragment sentence
+                    if(len(s))>1:
+                        for i in range(len(sentences)):
+                            sentences[i]+='\n'
+                        ds.writelines(sentences)
+                        
+                    line=f.readline()
+
+                ds.close()
             f.close()
 
-        '''except FileNotFoundError as e:      #Show traceback information if file was not found
-            err_msg=e.with_traceback(e.__traceback__)
-            log(err_msg)'''
+            '''except FileNotFoundError as e:      #Show traceback information if file was not found
+                err_msg=e.with_traceback(e.__traceback__)
+                log(err_msg)'''
 
 if __name__ == "__main__":
     data_set_generator.read_from_file(data_set_generator,str(os.getcwd()+"/article.txt"))
